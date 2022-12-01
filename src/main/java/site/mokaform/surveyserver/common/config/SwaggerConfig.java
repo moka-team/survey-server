fc7f7aedfb8513ea484b70444439548f41782bed
HyeonseoJUNG <windymay1920@gmail.com>
2022-12-01 15:04:23 +0900

    chore: swagger 설정 수정

--- src/main/java/site/mokaform/surveyserver/common/config/SwaggerConfig.java
+++ src/main/java/site/mokaform/surveyserver/common/config/SwaggerConfig.java
@@ -1,10 +1,7 @@
 package site.mokaform.surveyserver.common.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,33 +14,6 @@ public class SwaggerConfig {
                 .description("Team MOKA의 mokaform survey service API입니다.");
 
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("Access Token", getAccessTokenAuthScheme())
-                        .addSecuritySchemes("Refresh Token", getRefreshTokenAuthScheme()))
-                .addSecurityItem(getSecurityItem())
                 .info(info);
-    }
-
-    private SecurityScheme getAccessTokenAuthScheme() {
-        return new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
-    }
-
-    private SecurityScheme getRefreshTokenAuthScheme() {
-        return new SecurityScheme()
-                .type(SecurityScheme.Type.APIKEY)
-                .in(SecurityScheme.In.COOKIE)
-                .name("refreshToken");
-    }
-
-    private SecurityRequirement getSecurityItem() {
-        SecurityRequirement securityItem = new SecurityRequirement();
-        securityItem.addList("Access Token");
-        securityItem.addList("Refresh Token");
-        return securityItem;
     }
 }
